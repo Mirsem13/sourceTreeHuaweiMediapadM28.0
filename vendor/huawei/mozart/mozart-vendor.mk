fd51c9c6a760bcc32a6035ae938c4ce32374c147
Semyon <mirsem9@gmail.com>
2021-08-22 13:53:24 +0300

    new

---
+++ vendor/huawei/mozart/mozart-vendor.mk
@@ -0,0 +1,3 @@
+DEVICE_PACKAGE_OVERLAYS := vendor/huawei/mozart/overlay
+
+$(call inherit-product, vendor/huawei/mozart/mozart-vendor-blobs.mk)
--- vendor/huawei/mozart/system/etc/check_hota.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-hota_flag=/cache/recovery/last_recovery_log_upload
-[ -f "${hota_flag}" ] && echo "dsm_recovery,20900,recovery_fail" > /dev/dsm
